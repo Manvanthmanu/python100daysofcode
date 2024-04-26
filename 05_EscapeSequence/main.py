4d170173bc69c3b5683ba135c578a78424597416
Manvanth mn <92145905+Manvanthmanu@users.noreply.github.com>
2024-04-27 00:13:18 +0530

    done with day 3

---
+++ 05_EscapeSequence/main.py
@@ -0,0 +1,11 @@
+# Please do not change this code
+# hey there this is me 
+# hello there please shut up .
+
+'''
+what the fuck 
+did u mean when 
+u wannated to end this
+why did u ?
+'''
+print('hello world', end=" hello world")
---
+++ 06_VariablesDatatypes/main.py
@@ -0,0 +1,13 @@
+a = 10
+b='str'
+print(type(b))
+
+#  Data types
+
+# numerics - int , float , complex
+# character - str
+# boolean - bool
+# sequential - link tupple
+# mapped - dict
+
+# in python everything is an object
