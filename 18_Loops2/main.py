673efc95bcc26f4aa0df1d02259d68c213a41974
Manvanth mn <21sjccc039_manvanth@sjcc.edu.in>
2024-05-03 00:46:34 +0530

    done with day 8

---
+++ 18_Loops2/main.py
@@ -0,0 +1,16 @@
+#  while loop 
+
+# j = 0 
+# while(j<3):
+#     print(j)
+#     j = j+1
+
+
+code = int(input('Enter a 3 digit number : '))
+while(code<=100):
+    print('please input the correct number')
+    code = int(input('Enter a 3 digit number : '))
+else:
+    print('you enterd a 3 digit number ')
+
+# do while loop
