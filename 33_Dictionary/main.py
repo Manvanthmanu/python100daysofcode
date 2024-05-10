49481ddc728f2fdfa55c038502a27ebbe7a16aa0
Manvanth mn <21sjccc039_manvanth@sjcc.edu.in>
2024-05-11 01:44:13 +0530

    donewith day 17

---
+++ 33_Dictionary/main.py
@@ -0,0 +1,16 @@
+#  Dictionary in python 
+
+dic = {
+    "harry": "human being",
+    "Spoon" : "Object"
+}
+
+print(dic["harry"])
+
+print(dic.get('harry'))
+
+print(dic.keys())
+print(dic.values())
+
+for key,value in  dic.items():
+    print({value})
---
+++ 34_DictionaryMethods/main.py
@@ -0,0 +1,12 @@
+ep1 = {
+    123:45, 124: 89
+}
+
+ep2 = {222:67 , 566: 55}
+
+ep1.update(ep2)
+# clear
+#  pop
+#  pop item
+#  del
+print(ep1)
